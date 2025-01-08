2cfdeab20b4263665494a24dbc1cd58c14c1c6b4
Eduardo <echavezv@utea.edu.pe>
2025-01-08 07:19:07 -0500

    HomePage completo

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,69 +8,95 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 20.0,
-            ),
-            Container(
-              height: 200,
-              width: 200,
-              decoration: BoxDecoration(
-                image: const DecorationImage(
-                  image: AssetImage("assets/images/david.jpg"),
-                ),
-                borderRadius: BorderRadius.circular(20.0),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.5),
-                    offset: const Offset(5, 5),
-                    blurRadius: 12.0,
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 20.0,
+              ),
+              Container(
+                height: 200,
+                width: 200,
+                decoration: BoxDecoration(
+                  image: const DecorationImage(
+                    image: AssetImage("assets/images/david.jpg"),
                   ),
-                ],
-              ),
-            ),
-            const SizedBox(
-              height: 20.0,
-            ),
-            Text(
-              "Flutter Components",
-              style: GoogleFonts.poppins(
-                fontSize: 22.0,
-                letterSpacing: 1,
-              ),
-            ),
-            const SizedBox(
-              width: 160.0,
-              child: Divider(),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 12.0, vertical: 6.0),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(16.0),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.2),
-                    offset: Offset(4, 4),
-                    blurRadius: 12.0,
-                  ),
-                ],
-              ),
-              child: ListTile(
-                leading: Icon(
-                  Icons.check_circle_outline,
-                  color: Colors.black.withOpacity(0.2),
-                ),
-                title: Text("Avatar"),
-                subtitle: Text("Ir a detalle de Avatar"),
-                trailing: Icon(
-                  Icons.chevron_right_outlined,
-                  color: Colors.black.withOpacity(0.2),
+                  borderRadius: BorderRadius.circular(20.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.5),
+                      offset: const Offset(5, 5),
+                      blurRadius: 12.0,
+                    ),
+                  ],
                 ),
               ),
-            ),
-          ],
+              const SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                "Flutter Components",
+                style: GoogleFonts.poppins(
+                  fontSize: 22.0,
+                  letterSpacing: 1,
+                ),
+              ),
+              const SizedBox(
+                width: 160.0,
+                child: Divider(),
+              ),
+              ItemComponentWidget(title: "Avatar"),
+              ItemComponentWidget(title: "Alerts"),
+              ItemComponentWidget(title: "Cards"),
+              ItemComponentWidget(title: "Inputs"),
+              ItemComponentWidget(title: "Lists"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ItemComponentWidget extends StatelessWidget {
+  String title;
+
+  ItemComponentWidget({required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 6.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.2),
+            offset: const Offset(4, 4),
+            blurRadius: 12.0,
+          ),
+        ],
+      ),
+      child: ListTile(
+        leading: Icon(
+          Icons.check_circle_outline,
+          color: Colors.black,
+        ),
+        title: Text(
+          title,
+          style: GoogleFonts.poppins(
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        subtitle: Text(
+          "Ir a detalle de $title",
+          style: GoogleFonts.poppins(),
+        ),
+        trailing: Icon(
+          Icons.chevron_right_outlined,
+          color: Colors.black.withOpacity(0.2),
         ),
       ),
     );
