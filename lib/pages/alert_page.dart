9cd9a8eff586a2f8845df8b11df535d0d6b73d96
Eduardo <echavezv@utea.edu.pe>
2025-01-13 07:44:36 -0500

    Alert page completo

--- lib/pages/alert_page.dart
+++ lib/pages/alert_page.dart
@@ -1,13 +1,50 @@
 import 'package:flutter/material.dart';
 
 class AlertPage extends StatelessWidget {
-  const AlertPage({super.key});
+  showMyAlert1(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("Alert 1"),
+            content: Text("Hola este es el contenido del alert 1"),
+            backgroundColor: Colors.white,
+            actions: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text("Cancelar"),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("Aceptar"),
+              ),
+            ],
+          );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Alert Page"),
+        title: Text("Alert Page"),
+        backgroundColor: Colors.amberAccent,
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                showMyAlert1(context);
+              },
+              child: Text("Alert 1"),
+            ),
+          ],
+        ),
       ),
     );
   }
